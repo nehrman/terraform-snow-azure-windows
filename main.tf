5ddec7efa2f7b3b2b6bd1a9a17a2ddffe03ae851
nehrman <nicolas.ehrman@gmail.com>
2020-06-22 10:07:04 +0200

    0.12 refactoring

--- main.tf
+++ main.tf
@@ -1,26 +1,26 @@
 resource "azurerm_resource_group" "windows-rg" {
   name     = "${var.env}-${var.resource_group}"
-  location = "${var.location}"
+  location = var.location
 }
 
 resource "azurerm_virtual_network" "windows-vnet" {
   name                = "${var.env}-${var.virtual_network_name}"
-  location            = "${azurerm_resource_group.windows-rg.location}"
-  address_space       = ["${var.address_space}"]
-  resource_group_name = "${azurerm_resource_group.windows-rg.name}"
+  location            = azurerm_resource_group.windows-rg.location
+  address_space       = [var.address_space]
+  resource_group_name = azurerm_resource_group.windows-rg.name
 }
 
 resource "azurerm_subnet" "windows-subnet" {
   name                 = "${var.env}-${var.demo_prefix}subnet"
-  virtual_network_name = "${azurerm_virtual_network.windows-vnet.name}"
-  resource_group_name  = "${azurerm_resource_group.windows-rg.name}"
-  address_prefix       = "${var.subnet_prefix}"
+  virtual_network_name = azurerm_virtual_network.windows-vnet.name
+  resource_group_name  = azurerm_resource_group.windows-rg.name
+  address_prefixes     = var.subnet_prefix
 }
 
 resource "azurerm_network_security_group" "windows-sg" {
   name                = "${var.env}-${var.demo_prefix}-sg"
-  location            = "${azurerm_resource_group.windows-rg.location}"
-  resource_group_name = "${azurerm_resource_group.windows-rg.name}"
+  location            = azurerm_resource_group.windows-rg.location
+  resource_group_name = azurerm_resource_group.windows-rg.name
 
   security_rule {
     name                       = "winrm"
@@ -74,18 +74,18 @@ resource "azurerm_network_security_group" "windows-sg" {
 # A network interface. This is required by the azurerm_virtual_machine 
 # resource. Terraform will let you know if you're missing a dependency.
 resource "azurerm_network_interface" "windows-nic" {
-  count               = "${var.servers}"
+  count               = var.servers
   name                = "${var.demo_prefix}windows-nic-${count.index}"
-  location            = "${var.location}"
-  resource_group_name = "${azurerm_resource_group.windows-rg.name}"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.windows-rg.name
 
   # network_security_group_id = "${azurerm_network_security_group.ptfe-sg.id}"
 
   ip_configuration {
     name                          = "${var.demo_prefix}ipconfig-${count.index}"
-    subnet_id                     = "${azurerm_subnet.windows-subnet.id}"
+    subnet_id                     = azurerm_subnet.windows-subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = "${element(azurerm_public_ip.windows-pip.*.id, count.index)}"
+    public_ip_address_id          = element(azurerm_public_ip.windows-pip.*.id, count.index)
   }
 }
 
@@ -93,29 +93,29 @@ resource "azurerm_network_interface" "windows-nic" {
 # optionally add a public IP address for Internet-facing applications and 
 # demo environments like this one.
 resource "azurerm_public_ip" "windows-pip" {
-  count               = "${var.servers}"
+  count               = var.servers
   name                = "${var.demo_prefix}-ip-${count.index}"
-  location            = "${azurerm_resource_group.windows-rg.location}"
-  resource_group_name = "${azurerm_resource_group.windows-rg.name}"
+  location            = azurerm_resource_group.windows-rg.location
+  resource_group_name = azurerm_resource_group.windows-rg.name
   allocation_method   = "Dynamic"
   domain_name_label   = "${var.env}-${var.hostname}-${count.index}"
 }
 
 resource "azurerm_virtual_machine" "web_server" {
-  count                 = "${var.servers}"
-  name                  = "${var.hostname}-${count.index}"
-  location              = "${azurerm_resource_group.windows-rg.location}"
-  resource_group_name   = "${azurerm_resource_group.windows-rg.name}"
-  vm_size               = "Standard_D2s_v3"
+  count               = var.servers
+  name                = "${var.hostname}-${count.index}"
+  location            = azurerm_resource_group.windows-rg.location
+  resource_group_name = azurerm_resource_group.windows-rg.name
+  vm_size             = "Standard_D2s_v3"
 
-  network_interface_ids         = ["${element(azurerm_network_interface.windows-nic.*.id, count.index)}"]
+  network_interface_ids         = [element(azurerm_network_interface.windows-nic.*.id, count.index)]
   delete_os_disk_on_termination = "true"
 
   storage_image_reference {
     publisher = "MicrosoftWindowsServer"
     offer     = "WindowsServer"
     sku       = "2016-Datacenter"
-    version = "latest"
+    version   = "latest"
   }
 
   storage_os_disk {
@@ -126,16 +126,16 @@ resource "azurerm_virtual_machine" "web_server" {
   }
 
   tags = {
-    name  = "Nicolas Ehrman"
-    ttl   = "13"
-    owner = "nehrman@hashicorp.com"
+    name       = "Nicolas Ehrman"
+    ttl        = "13"
+    owner      = "nehrman@hashicorp.com"
     created_by = "Service Now"
   }
 
   os_profile {
     computer_name  = "${var.hostname}-${count.index}"
-    admin_username = "${var.admin_username}"
-    admin_password = "${var.admin_password}"
+    admin_username = var.admin_username
+    admin_password = var.admin_password
   }
 
   os_profile_windows_config {
@@ -157,7 +157,7 @@ resource "azurerm_virtual_machine" "web_server" {
       pass         = "oobeSystem"
       component    = "Microsoft-Windows-Shell-Setup"
       setting_name = "FirstLogonCommands"
-      content      = "${file("${path.module}/scripts/FirstLogonCommands.xml")}"
+      content      = file("${path.module}/scripts/FirstLogonCommands.xml")
     }
   }
 }
